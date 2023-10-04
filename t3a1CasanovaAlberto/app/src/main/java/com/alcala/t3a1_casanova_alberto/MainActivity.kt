35eb14ca20925b1b71e1f94c5ec6a80af28ead86
RanetaCroak <62992222+RanetaCroak@users.noreply.github.com>
2023-10-04 20:30:05 +0200

    Add files via upload

---
+++ t3a1CasanovaAlberto/app/src/main/java/com/alcala/t3a1_casanova_alberto/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.alcala.t3a1_casanova_alberto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
